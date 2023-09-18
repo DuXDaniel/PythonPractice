db19cd8af3846ebad1061e459b508f8775d50b2f
Daniel X. Du <du.daniel.x@gmail.com>
2023-09-18 17:24:11 -0400

    Update ML_Play_Sentiment.py

--- ML_Play_Sentiment.py
+++ ML_Play_Sentiment.py
@@ -11,6 +11,7 @@ from tensorflow.keras import losses
 batch_size = 32
 seed = 42
 
+'''
 raw_train_ds = tf.keras.utils.text_dataset_from_directory(
     'aclImdb/train', 
     batch_size=batch_size, 
@@ -46,6 +47,7 @@ def custom_standardization(input_data):
     return tf.strings.regex_replace(stripped_html,
     '[%s]' % re.escape(string.punctuation),
     '')
+'''
 
 max_features = 10000
 sequence_length = 250
